cc532aa24b3d886407e35747f879f50035528f1f
yakub nasir <yakubnasir8@gmail.com>
2024-02-09 05:18:00 -0800

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,26 +1,31 @@
 let menu = document.querySelector('#bars');
 let navBar = document.querySelector('.navbar');
 menu.onclick = () => {
-menu.classList.toggle('fa-circle-xmark');
-navBar.classList.toggle('active');
+  menu.classList.toggle('fa-circle-xmark');
+  navBar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-circle-xmark');
-    navBar.classList.remove('active');
+window.onscroll = () => {
+  menu.classList.remove('fa-circle-xmark');
+  navBar.classList.remove('active');
 }
- document.querySelector('#searchicon').onclick = () =>{
-    document.querySelector('#search-form').classList.toggle('active')
- }    
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active')
+document.querySelector('#searchicon').onclick = () => {
+  document.querySelector('#search-form').classList.toggle('active')
 }
-var swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
+document.querySelector('#close').onclick = () => {
+  document.querySelector('#search-form').classList.remove('active')
+}
+var swiper = new Swiper(".swiper-container", {
+  spaceBetween: 120,
   centeredSlides: true,
-  cssMode: true,
+  autoplay: {
+    delay: 7500,
+    disbaleOnInteraction: false,
+  },
+
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   mousewheel: true,
   keyboard: true,
